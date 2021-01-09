5a0fc6149a3b81005d0ae69dcad22683f542cb81
TravisLovingood <61667953+TravisLovingood@users.noreply.github.com>
2021-01-09 17:20:00 -0500

    updated contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../App.css"
 // Bootstrap stuff
-import Image from 'react-bootstrap/Image'
+// import Image from 'react-bootstrap/Image'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -29,25 +29,10 @@ function Contact() {
         <Col></Col>
         <Col md={6}>
           {/* <Card className="p-3"> */}
-            <Row>
-              {/* <Col></Col> */}
-
-              <Col md={6}>
-                <Card.Text>
-                  <Image src="pics/Logo-Pink.PNG" alt="Historians For a Better Future Logo" rounded fluid/>
-                </Card.Text>
-              </Col>
-
-              <Col md={6}>
-                <Card.Text>Filler Text - Lorem ipsum dolor sit amet, consectetur 
-                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                  exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-                  consequat.                          
-                </Card.Text>
-              </Col>
-              {/* <Col></Col> */}
-            </Row>
+          <Card.Text>
+            Interested in learning more or becoming involved in HBF? Email us or 
+            check out our content on social media:      
+          </Card.Text>
           {/* </Card>     */}
         </Col>
         <Col></Col>
@@ -59,38 +44,17 @@ function Contact() {
         <Col></Col>
         <Col md={8}>
           <Card className="p-3">
-            <Row>
-              <Col></Col>
-
-              <Col md={5}>
-                <Card.Title><h5><strong>Direct Contacts</strong></h5></Card.Title>
-                <hr />
-                <Card.Text>
-                  <li><h3><a href="https://github.com/TravisLovingood">link1</a></h3></li>                       
-                  <li><h3><a href="https://github.com/TravisLovingood">link2</a></h3></li>
-                  <li><h3><a href="https://github.com/TravisLovingood">link3</a></h3></li>
-                  <li><h3><a href="https://github.com/TravisLovingood">link4</a></h3></li>                       
-                  <li><h3><a href="https://github.com/TravisLovingood">link5</a></h3></li>
-                  <li><h3><a href="https://github.com/TravisLovingood">link6</a></h3></li>
-                </Card.Text>
-              </Col>
-
-              <Col md={5}>
-                <Card.Title><h5><strong>Additional Materials</strong></h5></Card.Title>
-                <hr />
-                <Card.Text>
-                  <li><h3><a href="https://github.com/TravisLovingood">link1</a></h3></li>                       
-                  <li><h3><a href="https://github.com/TravisLovingood">link2</a></h3></li>
-                  <li><h3><a href="https://github.com/TravisLovingood">link3</a></h3></li>
-                  <li><h3><a href="https://github.com/TravisLovingood">link4</a></h3></li>                       
-                  <li><h3><a href="https://github.com/TravisLovingood">link5</a></h3></li>
-                  <li><h3><a href="https://github.com/TravisLovingood">link6</a></h3></li>
-                </Card.Text>
-              </Col>
-
-              <Col></Col>
-            </Row>
-          </Card>    
+            <Card.Text>
+              <h5>Email:</h5> 
+              <li><h3>historiansforabetterfuture@gmail.com</h3></li>   
+            </Card.Text><hr/>
+            <Card.Text> 
+              <h5>Social Media:</h5>                               
+              <li><h3><a href="https://facebook.com/hist4future/">Facebook: facebook.com/hist4future/</a></h3></li>
+              <li><h3><a href="https://twitter.com/hist4future @Hist4Future">Twitter: twitter.com/hist4future @Hist4Future</a></h3></li>
+              <li><h3>Instagram: hist4future</h3></li>
+            </Card.Text>
+          </Card>   
         </Col>
         <Col></Col>
       </Row>
